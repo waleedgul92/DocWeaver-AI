d991127b508c5b9457cebee4cac3658b455d0a69
waleedgul92 <hwaleed0035@google.com>
2024-11-13 13:34:42 +0500

    added FAIS and GoogleAI and Chatgpt embeddings

--- Code/preprocess_and_store_data.py
+++ Code/preprocess_and_store_data.py
@@ -1,16 +1,19 @@
-import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
-import os
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
 import langchain.vectorstores as vectorstore
 import chromadb
 import bs4
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS  
+import faiss
+from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-def scrap_url_data(url,model):
+def scrap_url_data(url):
 
     # Read the page source
     loader=WebBaseLoader( web_paths=url,
@@ -22,10 +25,11 @@ def scrap_url_data(url,model):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunked_text=text_splitter.split_text(page)
 
+    return chunked_text
 
 
 
-def preprocess_document_data(document_path,model):
+def preprocess_document_data(document_path):
     pdf_loader = PyPDFLoader(document_path)
     pages = pdf_loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
@@ -47,3 +51,16 @@ def preprocess_document_data(document_path,model):
 
 
     return chunked_documents
+
+
+
+def generate_embedding_and_store(text, model):
+    if model=="ollama":
+        embedding_method = OllamaEmbeddings()
+    elif model=="gemini":
+        embedding_method=GoogleGenerativeAIEmbeddings()
+    elif model=="chatgpt":
+        embedding_method = OpenAIEmbeddings()
+
+    db=FAISS.from_documents(text,embedding_method)
+    return db
